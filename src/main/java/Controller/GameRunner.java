fc27d50888d0acd36ab1f40053821fb04dc1973d
Kabilan <gnanavarothayan@student.tudelft.nl>
2017-09-09 14:58:45 +0200

    PlayerballController added that launches the ball and reflects it from the wall

--- src/main/java/Controller/GameRunner.java
+++ src/main/java/Controller/GameRunner.java
@@ -4,31 +4,38 @@ import java.awt.event.*;
 import javax.swing.Timer;
 import View.View;
 
+
 /**
  * Created by jur on 9/5/2017.
  */
 public class GameRunner {
 
-        private final Timer timer = new Timer(40,null);
-        private GridController gridController;
-        private View view;
+    private final Timer timer = new Timer(40,null);
+    private GridController gridController;
+    private PlayerBallController ballController;
+    private View view;
 
-        private void initializeGameTimer(){
-            timer.addActionListener(new ActionListener() {
+    private void initializeGameTimer() {
+        timer.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent evt) {
                     gridController.process();
+                    if (ballController.getMouseY() != 0) {
+                        ballController.launchBall();
+                    }
                     view.redraw();
                 }
             });
-        }
+    }
 
-        public void runGame(){
-            initializeGameTimer();
-            timer.start();
-        }
+    public void runGame() {
+        initializeGameTimer();
+        timer.start();
+    }
 
-        public GameRunner(GridController gridController, View view){
-            this.gridController = gridController;
-            this.view = view;
-        }
+    public GameRunner(GridController gridController, View view,
+                          PlayerBallController ballController) {
+        this.gridController = gridController;
+        this.view = view;
+        this.ballController = ballController;
+    }
 }
--- src/main/java/Controller/PlayerBallController.java
+++ src/main/java/Controller/PlayerBallController.java
@@ -1,63 +1,159 @@
 package Controller;
 
-import Elements.Element;
 import Elements.Player;
 import Elements.PlayerBall;
-import Model.Cell;
 import Model.Grid;
 import View.View;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 
-import java.util.ArrayList;
-
+/**
+ * The playerballController keeps track of the movement of the ball.
+ */
 public class PlayerBallController {
 
-    private MouseEvent mouse;
-    public final int ballRadius = 15;
+    private final int ballRadius = 15;
+    private Player player;
+    private Grid grid;
+    private double mouseX;
+    private double mouseY;
+    private double deltaX;
+    private double deltaY;
+    private int counter;
 
-    public void launchBall(Player player, Grid grid) {
 
-        if (!mouse.isPrimaryButtonDown()&&player.getPlayerBall().hasCollidedWithCell(grid)) {
+    /**
+     * The playerballController consists of the player and the grid of the game and an counter
+     *  which keeps track of the times the mouse has been clicked.
+     * @param player the player object of the game.
+     * @param grid the grid of the game.
+     */
+    public PlayerBallController(Player player, Grid grid) {
+        this.player = player;
+        this.grid = grid;
+        counter = 0;
+    }
+
+    /**
+     * After the mouse position is given, it will calculate the deltaX and deltaY by creating
+     * a vector between the mouse position and playerball position.
+     * Then you will divide both of the X and Y coordinate of the vector with the highest value
+     * of either the X and Y so that you will have a smaller Delta.
+     */
+    public void calculateDelta() {
+        if (counter > 0) {
             return;
         }
-        double vectorX = mouse.getSceneX() - player.getPlayerBall().getX();
-        double vectorY = mouse.getSceneY() - player.getPlayerBall().getY();
-        double max = Math.max(vectorX, vectorY);
-        double deltaX = vectorX / max;
-        double deltaY = vectorY / max;
 
-        final int maximumTimesBallHit = 4;
-        while (player.getPlayerBall().getCounter() < maximumTimesBallHit) {
-            if (player.getPlayerBall().hasCollidedWithWall()) {
-                reflectBack(player, deltaX, deltaY);
-            }
-            double newXCoord = player.getPlayerBall().getX() + deltaX;
-            double newYCoord = player.getPlayerBall().getY() + deltaY;
+        final int speedup = 5;
+        double vectorX = mouseX - player.getPlayerBall().getX();
+        double vectorY = mouseY - player.getPlayerBall().getY();
+        double max = Math.max(Math.abs(vectorX), Math.abs(vectorY));
 
-            player.getPlayerBall().setX(newXCoord);
-            player.getPlayerBall().setY(newYCoord);
-
-            if (player.getPlayerBall().hasCollidedWithCell(grid)) {
-                break;
-            }
+        if (max > 0) {
+            deltaX = speedup * vectorX / max;
+            deltaY = speedup * vectorY / max;
+            counter++;
         }
 
-        player.setPlayerBall(new PlayerBall(View.STAGE_WIDTH / 2, View.TOP_BAR_HEIGHT));
+    }
+
+    /**
+     * Launches the ball in the direction of the mouse.
+     */
+    public void launchBall() {
+        // if the ball has collided with the wall the deltaX or deltaY will become negative.
+        if (player.getPlayerBall().hasCollidedWithWall()) {
+            double[] newDelta = reflectBack(deltaX, deltaY);
+            deltaX = newDelta[0];
+            deltaY = newDelta[1];
+        }
+
+        // the new coordinate of the ball is the the previous one added with the delta.
+        double newXCoord = player.getPlayerBall().getX() + deltaX;
+        double newYCoord = player.getPlayerBall().getY() + deltaY;
+
+        // set the new coordinate for the playerball.
+        player.getPlayerBall().setX(newXCoord);
+        player.getPlayerBall().setY(newYCoord);
+
+
+        // if the wall has collided with the wall for a maximum of 3 times then it will reset
+        // the ball
+        final int  maximumTimesBallHit = 3;
+        if (player.getPlayerBall().getCounter() >= maximumTimesBallHit) {
+            player.setPlayerBall(new PlayerBall(View.STAGE_WIDTH / 2 - View.SCREEN_WITH_DEVIATION,
+                    View.TOP_BAR_HEIGHT));
+            this.setMouseY(0);
+            this.setMouseX(0);
+            this.setDeltaX(0);
+            this.setDeltaY(0);
+            counter = 0;
+        }
 
 
     }
 
-    private void reflectBack(Player player, double deltaX, double deltaY) {
-        if ((player.getPlayerBall().getX() <= ballRadius)
+    /**
+     * When the ball collides with the wall, then it will make deltaX negative if it collided
+     * with the walls in the width and deltaY negative if it collided with the s in the height.
+     * @param deltaX the X direction in which the ball moves.
+     * @param deltaY the Y direction in whch the ball moves.
+     * @return an array with the negative of either or both of the deltaX and deltaY.
+     */
+    public double[] reflectBack(double deltaX, double deltaY) {
+
+        if ((player.getPlayerBall().getX() < ballRadius - View.SCREEN_WITH_DEVIATION)
                 || (player.getPlayerBall().getX() >= View.STAGE_WIDTH - ballRadius)) {
             deltaX = deltaX * -1;
         }
-        if ((player.getPlayerBall().getY() <= View.TOP_BAR_HEIGHT + ballRadius)
-                || (player.getPlayerBall().getX() >= View.STAGE_HEIGHT - ballRadius)) {
+        if ((player.getPlayerBall().getY() < View.TOP_BAR_HEIGHT)
+                || (player.getPlayerBall().getY() >= View.STAGE_HEIGHT - ballRadius)) {
             deltaY = deltaY * -1;
         }
+        return new double [] {deltaX, deltaY};
     }
 
+    /**
+     * @return the X coordinate of the mouse
+     */
+    public double getMouseX() {
+        return mouseX;
+    }
+
+    /**
+     * @param mouseX set the X coordinate of the mouse
+     */
+    public void setMouseX(double mouseX) {
+        this.mouseX = mouseX;
+    }
+
+    /**
+     * @return the Y coordinate of the mouse
+     */
+    public double getMouseY() {
+        return mouseY;
+    }
+
+    /**
+     * @param mouseY set the Y coordinate of the mouse
+     */
+    public void setMouseY(double mouseY) {
+        this.mouseY = mouseY;
+    }
+
+
+    /**
+     * @param deltaX the X translation of the ball
+     */
+    public void setDeltaX(double deltaX) {
+        this.deltaX = deltaX;
+    }
+
+
+    /**
+     * @param deltaY the Y translation of the ball
+     */
+    public void setDeltaY(double deltaY) {
+        this.deltaY = deltaY;
+    }
 
 }
