3a63f90752be9b23a4946dde0385b399fcd8ac58
J.Theunisz <J.Theunisz@student.tudelft.nl>
2017-09-14 13:24:44 +0200

    main screen and background music

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -4,9 +4,14 @@ import Model.GameData;
 import View.View;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 /**
  * Created by jur on 9/14/2017.
@@ -24,6 +29,12 @@ public class GameController {
     private Scene mainMenu;
     private Scene gameScreen;
     private Pane gamePane;
+    private Pane mainMenuPane;
+
+    private Media backgroundMusic;
+    private MediaPlayer mediaPlayer;
+    private int backgroundMusicVolume;
+
 
     public GameController(Stage primaryStage){
         this.primaryStage = primaryStage;
@@ -39,6 +50,7 @@ public class GameController {
         gridController = new GridController(data.getGrid());
         playerBallController = new PlayerBallController(data.getPlayer(), data.getGrid());
 
+        setupSound();
         setupView();
 
         //Start running the game
@@ -49,8 +61,36 @@ public class GameController {
     }
 
     private void setMouseControllers(){
-        // Create a mousebutton event that keeps track on when the mouse has been clicked,
-        // so that the ball will move in that direction.
+        // play button event
+        view.getPlayButton().setOnMouseReleased(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                primaryStage.setScene(gameScreen);
+            }
+        });
+
+        //exit button event
+        view.getExitButton().setOnMouseReleased(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                primaryStage.close();
+                System.exit(0);
+            }
+        });
+
+        //sound button event
+        view.getMusicButton().setOnMouseReleased(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                handleMusicButtonClick();
+            }
+        });
+
+        //sound button event
+        view.getMusicIcon().setOnMouseReleased(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                handleMusicButtonClick();
+            }
+        });
+
+        // ball firing event
         gameScreen.setOnMouseReleased(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
                 playerBallController.setMouseX(event.getSceneX());
@@ -60,20 +100,44 @@ public class GameController {
         });
     }
 
+    private void setupSound(){
+        backgroundMusicVolume = 100;
+        backgroundMusic = new Media(new File("src/main/resources/sounds/bgm1.mp3").toURI().toString());
+        mediaPlayer = new MediaPlayer(backgroundMusic);
+        mediaPlayer.play();
+    }
+
     private void setupView(){
-        //Create a pane to hold the game
+        //Create the panes to hold game elements
+        mainMenuPane = new Pane();
         gamePane = new Pane();
 
         //Draw elements on pane
-        view = new View(gamePane, data);
+        view = new View(mainMenuPane, gamePane, data);
+        view.drawMainMenu();
         view.drawGame();
 
-        // Create a scene containing the pane and place it in the stage
+        // Create scenes containing the panes
+        mainMenu = new Scene(mainMenuPane, View.STAGE_WIDTH, View.STAGE_HEIGHT);
         gameScreen = new Scene(gamePane, View.STAGE_WIDTH, View.STAGE_HEIGHT);
+
+        // Add the scenes to the stage
         primaryStage.setTitle("Bubble Shooter"); // Set the stage title
-        primaryStage.setScene(gameScreen); // Place the scene in the stage
+        primaryStage.setScene(mainMenu); // Place the scene in the stage
         primaryStage.setResizable(false); //cannot resize game
         primaryStage.show(); // Display the stage
+    }
+
+    private void handleMusicButtonClick(){
+        view.changeMusicButton(backgroundMusicVolume);
+        if(backgroundMusicVolume == 100){
+            backgroundMusicVolume = 0;
+            mediaPlayer.pause();
+        }
+        else {
+            backgroundMusicVolume = 100;
+            mediaPlayer.play();
+        }
     }
 
     public static View getView() {
--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -16,34 +16,86 @@ import java.util.ArrayList;
 public class View {
 
     private GameData data;
-    private Pane gamePane;
 
-    private Image background;
+    private Pane gamePane;
+    private Pane mainMenuPane;
+
+    private Image mainMenuBg;
+    private ImageView playButton;
+    private ImageView exitButton;
+    private ImageView musicButton;
+    private ImageView settingsButton;
+    private Image musicIconMax;
+    private Image musicIconMin;
+    private ImageView musicIcon;
+    private ImageView settingsIcon;
+
+    private Image gameBg;
     private ImageView topBar;
     private ImageView scoreBar;
 
-
-
     private ImageView playerBall;
     private ImageView nextBall;
-    private ImageView fake;
 
     public static final double STAGE_WIDTH = 600;
-    public static double STAGE_HEIGHT = 700;
+    public static final double STAGE_HEIGHT = 700;
     public static final int TOP_BAR_HEIGHT = 70;
-    private static final int SCORE_BAR_HEIGHT = 40;
-    private static final int SCORE_BAR_WIDTH = 240;
+    public static final int SCORE_BAR_HEIGHT = 40;
+    public static final int SCORE_BAR_WIDTH = 240;
 
-
-    public View(Pane gamePane, GameData data){
+    public View(Pane mainMenuPane, Pane gamePane, GameData data){
+        this.mainMenuPane = mainMenuPane;
         this.gamePane = gamePane;
         this.data = data;
     }
 
+    public void drawMainMenu(){
+        //draw background
+        mainMenuBg = new Image("images/main-menu-bg.png");
+        mainMenuPane.setBackground(new Background(new BackgroundImage(mainMenuBg, BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+
+        //draw buttons
+        playButton = new ImageView("images/play-button.png");
+        playButton.relocate(184, 260);
+        playButton.fitWidthProperty().setValue(250);
+        playButton.fitHeightProperty().setValue(80);
+        exitButton = new ImageView("images/exit-button.png");
+        exitButton.relocate(184, 370);
+        exitButton.fitWidthProperty().setValue(250);
+        exitButton.fitHeightProperty().setValue(80);
+        settingsButton = new ImageView("images/backButton.png");
+        settingsButton.relocate(20, 600);
+        settingsButton.fitWidthProperty().setValue(100);
+        settingsButton.fitHeightProperty().setValue(100);
+        musicButton = new ImageView("images/backButton.png");
+        musicButton.relocate(490, 600);
+        musicButton.fitWidthProperty().setValue(100);
+        musicButton.fitHeightProperty().setValue(100);
+        settingsIcon = new ImageView("images/settings-icon.png");
+        settingsIcon.relocate(40, 620);
+        settingsIcon.fitWidthProperty().setValue(60);
+        settingsIcon.fitHeightProperty().setValue(60);
+        musicIconMax = new Image("images/sound-icon-max.png");
+        musicIconMin = new Image("images/sound-icon-min.png");
+        musicIcon = new ImageView(musicIconMax);
+        musicIcon.relocate(510, 620);
+        musicIcon.fitWidthProperty().setValue(60);
+        musicIcon.fitHeightProperty().setValue(60);
+
+        //add components to main menu
+        mainMenuPane.getChildren().add(playButton);
+        mainMenuPane.getChildren().add(exitButton);
+        mainMenuPane.getChildren().add(settingsButton);
+        mainMenuPane.getChildren().add(musicButton);
+        mainMenuPane.getChildren().add(settingsIcon);
+        mainMenuPane.getChildren().add(musicIcon);
+    }
+
     public void drawGame(){
         //draw background
-        background = new Image("images/background1.png");
-        gamePane.setBackground(new Background(new BackgroundImage(background, BackgroundRepeat.NO_REPEAT,
+        gameBg = new Image("images/background1.png");
+        gamePane.setBackground(new Background(new BackgroundImage(gameBg, BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
 
         //draw top bar
@@ -135,8 +187,6 @@ public class View {
 
     }
 
-    // This method displays a ball when it has hit the hexagon
-    // Without this function you get a 1sec in which the ball is at (0,0)
     public void display(Cell c) {
         c.getElement().getImageView().relocate(getScreenX(c), getScreenY(c));
     }
@@ -147,6 +197,34 @@ public class View {
 
     private double getScreenY(Cell cell){
         return (cell.getCurrentY() - (cell.getElement().getSprite().getHeight() / 2));
+    }
+
+    public ImageView getPlayButton(){
+        return playButton;
+    }
+
+    public ImageView getExitButton(){
+        return exitButton;
+    }
+
+    public ImageView getSettingsButton() {
+        return settingsButton;
+    }
+
+    public ImageView getMusicButton() {
+        return musicButton;
+    }
+
+    public ImageView getMusicIcon() {
+        return musicIcon;
+    }
+
+    public void changeMusicButton(int amount) {
+        if(amount == 100) musicIcon.setImage(musicIconMin);
+        if(amount == 0) musicIcon.setImage(musicIconMax);
+        musicIcon.relocate(510, 620);
+        musicIcon.fitWidthProperty().setValue(60);
+        musicIcon.fitHeightProperty().setValue(60);
     }
 
 }
