2e0015199fd45c6ba91317b1238810148f3fe0b3
Hendrik Janse <hendrikjanse@live.com>
2017-09-15 14:44:36 +0200

    added Working test cases

---
+++ src/test/java/Model/GridTest.java
@@ -0,0 +1,77 @@
+package Model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+
+/**
+ * Created by Henks Laptop on 07/09/2017.
+ */
+class GridTest {
+
+
+    private Grid grid;
+    private int HEIGHT = 20;
+    private int WIDTH = 10;
+
+    @BeforeEach
+    private void setUp() {
+        grid = new Grid(WIDTH, HEIGHT);
+    }
+
+
+    @Test
+    void getCells() {
+
+    }
+
+    @Test
+    void setRotation() {
+    }
+
+    @Test
+    void getRotation() {
+    }
+
+    @Test
+    void getRotationDifference() {
+    }
+
+    @Test
+    void setRotationDifference() {
+    }
+
+    @Test
+    void getRotationSpeed() {
+    }
+
+    @Test
+    void setRotationSpeed() {
+    }
+
+    @Test
+    void getOccupiedCells() {
+    }
+    //@Test
+    void closestCellToLocation() {
+        assertThat(grid.closestCellToLocation(WIDTH, HEIGHT)).isEqualTo(grid.getCenterCell());
+    }
+
+    //@Test
+    void closestEmptyCellToLocation() {
+        assertThat(grid.closestEmptyCellToLocation(WIDTH, HEIGHT)).isEqualTo(grid.getCenterCell());
+    }
+
+
+    @Test
+    void getCenterCell() {
+
+    }
+
+}
