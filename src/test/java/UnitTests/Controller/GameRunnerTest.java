78058acfb3d84750ca5dcd03ce31c16b5ff109b8
Hendrik Janse <hendrikjanse@live.com>
2017-09-29 21:39:19 +0200

    added game tests

--- src/test/java/UnitTests/Controller/GameRunnerTest.java
+++ src/test/java/UnitTests/Controller/GameRunnerTest.java
@@ -1,34 +1,51 @@
 package UnitTests.Controller;
 
+import Controller.GameRunner;
 import Controller.GridController;
 import Controller.PlayerBallController;
+import Model.GameData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import View.View;
+import org.mockito.Mockito;
 
+import javax.swing.Timer;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
- * This class provides test cases for the GameRunner class.
+ * Created by Henks Laptop on 29/09/2017.
  */
 class GameRunnerTest {
-    GridController gridController = mock(GridController.class);
-    View view = mock(View.class);
-    PlayerBallController playerController = mock(PlayerBallController.class);
+    GameRunner gameRunner;
+    Timer timer;
 
+    @BeforeEach
+    void setUp() {
+        timer = mock(Timer.class);
+        gameRunner = new GameRunner(mock(GridController.class), mock(PlayerBallController.class), timer);
+    }
 
     @Test
     void runGame() {
-
+        gameRunner.runGame();
+        verify(timer).start();
     }
 
-    /**
-     * Created by Henks Laptop on 28/09/2017.
-     */
-    static class GameDataLoaderTest {
-        @Test
-        void initialize() {
-        }
-
+    @Test
+    void pauseGame() {
+        gameRunner.pauseGame();
+        verify(timer).stop();
     }
+
+    @Test
+    void continueGame() {
+        gameRunner.runGame();
+        verify(timer).start();
+    }
+
+
 }
