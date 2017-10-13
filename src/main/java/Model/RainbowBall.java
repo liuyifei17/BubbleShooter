36a894f103bc099b1f081690d0a33297729c6905
Yifei Liu <Y.Liu-27@student.tudelft.nl>
2017-10-13 15:39:45 +0200

    add Factories

---
+++ src/main/java/Model/RainbowBall.java
@@ -0,0 +1,10 @@
+package Model;
+
+/**
+ * A rainbow playerBall, it's a special ball.
+ */
+public class RainbowBall extends PlayerBall {
+    public RainbowBall(String color) {
+        super(color);
+    }
+}
